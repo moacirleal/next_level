975c6865c904218c8f7a3268889fa63499436c5e
moacir.silva <moacir.silva@infostore.com.br>
2020-08-04 22:56:50 -0400

    Primeira versão incompleta do site de aulas proffy

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import LogoImg from '../../assets/images/logo.svg'
+import LandingImg from '../../assets/images/landing.svg'
+import studyIcon from '../../assets/images/icons/study.svg'
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import './styles.css';
+import {Link} from 'react-router-dom'
+
+export default function Landing() {
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div id="logo-container">
+                    <img src={LogoImg} alt="Proffy" />
+                    <h2>Sua plataforma de estudos online</h2>
+                </div>
+                <img src={LandingImg} alt="Plataforma de estudos" className="hero-image" />
+                <div className="buttons-container">
+                    <Link to="./study" className="study">
+                        <img src={studyIcon} alt="Estudar" />
+                        Estudar
+                    </Link>
+                    <Link to="./give-classes" className="give-classes">
+                        <img src={giveClassesIcon} alt="Dar aula" />
+                        Dar aula
+                    </Link>
+                </div>
+                <span className="total-connections">
+                    Total de 200 conexões já realizadas <img src={purpleHeartIcon}
+                        alt="Coração roxo"></img>
+                </span>
+            </div>
+        </div>
+    );
+}
---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import './styles.css'; //importação de estilos
+import PageHeader from '../../components/PageHeader';
+
+export default function TeacherList() {
+    return (
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que incrível que você quer dar aulas." />
+        </div>
+    );
+}
