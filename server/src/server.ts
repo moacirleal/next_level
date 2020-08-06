3fef2836e0ee750dbbd90eddc238e1d245f2c1d7
moacir.silva <moacir.silva@infostore.com.br>
2020-08-05 23:37:38 -0400

    Conclusão do backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express'; //importação do pacote express
+import routes from './routes';
+import cors from 'cors';
 
 const app = express(); // criar uma constante que recebe o pacote express
 
@@ -6,23 +8,8 @@ const app = express(); // criar uma constante que recebe o pacote express
 //Route Params: Identificar qual recurso atualizar ou deletar
 //Query Params: Paginação, filtros, ordenação
 
-
-app.use(express.json()) //Informa ao express que a cada requesição do body ele deve retornar JSON
-
-app.get('/users', (request, response) => { //Informa o método, o endpoint e passa como parâmetros a resposta e a requisição
-    console.log(request.query)
-    
-    const users = [ //Cria um array que pega a estrutura do JSON
-        {
-            name: 'Moacir', age: 27
-        },
-        {
-            name: 'Eloise', age: 26
-        }
-    ];
-    return (
-        response.json([users]) //retorna para o navegador o o array transformardo em JSON
-    );
-})
+app.use(cors());
+app.use(express.json()); //Informa ao express que a cada requesição do body ele deve retornar JSON
+app.use(routes)
 
 app.listen(3333);
