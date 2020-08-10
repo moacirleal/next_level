5aed5ab7180843251803f37156a9905e3f7010f0
moacir.silva <moacir.silva@infostore.com.br>
2020-08-09 22:28:23 -0400

    Incluindo a versao mobile

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
+import api from '../../services/api';
 
-export default function TeacherItem() {
+export interface Teacher {
+    name: string;
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    subject: string;
+    whatsapp: string;
+}
+
+
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/68063282?s=460&v=4" alt="Walter White" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Walter White</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entusiata das melhores tecnologias de química avançada.
-                <br />
-                <br />
-            Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências.
-            Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-        </p>
+            <p>{teacher.bio}</p>
             <footer>
                 <p>
-                    Preço por hora <strong>R$ 20,00</strong>
+                    Preço/hora
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp" />
                 Entrar em contato
-            </button>
+            </a>
             </footer>
         </article>
     )
 }
+
+export default TeacherItem
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LogoImg from '../../assets/images/logo.svg'
 import LandingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
@@ -6,8 +6,20 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 import './styles.css';
 import {Link} from 'react-router-dom'
+import api from "../../services/api";
 
 export default function Landing() {
+    
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data
+
+            setTotalConnections(total);
+        })
+    }, [])
+    
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -27,7 +39,7 @@ export default function Landing() {
                     </Link>
                 </div>
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon}
+                    Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon}
                         alt="Coração roxo"></img>
                 </span>
             </div>
